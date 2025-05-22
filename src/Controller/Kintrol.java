5384217c8bfdf3e5ccf82430f33ecdd6406de18d
Sharon Cruz <sharina.1401@gmail.com>
2025-05-21 21:48:14 -0500

    llamado a la interfaz principal

--- src/Controller/Kintrol.java
+++ src/Controller/Kintrol.java
@@ -2,7 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package kintrol;
+package Controller;
+
+import View.PrincipalKintrol;
 
 /**
  *
@@ -15,6 +17,13 @@ public class Kintrol {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        //Interfaz principal
+        javax.swing.SwingUtilities.invokeLater(()->{
+            PrincipalKintrol principal = new PrincipalKintrol();
+            principal.setVisible(true);
+            principal.setLocationRelativeTo(null);
+        });
     }
     
 }
